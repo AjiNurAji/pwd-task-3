85bcb7a14501d86d5df012b6641a2f06507a12b2
AjiNurAji <ajinuraji090306@gmail.com>
2025-09-01 18:47:15 +0700

    initial commit

---
+++ assets/js/button.js
@@ -0,0 +1,31 @@
+// theme default
+document.addEventListener("DOMContentLoaded", () => {
+  const storedTheme = localStorage.getItem("theme");
+
+  if (storedTheme === "dark") {
+    doc.classList.add("dark")
+  }
+})
+
+// theme toggle
+const themeToggle = document.getElementById("theme-toggle");
+const doc = document.body;
+
+themeToggle.addEventListener("click", () => {
+	doc.classList.toggle("dark");
+
+	if (doc.classList.contains("dark")) {
+		localStorage.setItem("theme", "dark");
+	} else {
+		localStorage.setItem("theme", "light");
+	}
+});
+
+// hamburger toggle
+const nav = document.querySelector("#nav ul");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+	nav.classList.toggle("show");
+	hamburger.classList.toggle("active");
+});
