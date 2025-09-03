eb7d1015e526d8063d3a3b0f5e3d8254db66b870
AjiNurAji <ajinuraji090306@gmail.com>
2025-09-03 10:53:53 +0700

    update

--- assets/js/button.js
+++ assets/js/button.js
@@ -1,11 +1,11 @@
 // theme default
 document.addEventListener("DOMContentLoaded", () => {
-  const storedTheme = localStorage.getItem("theme");
+	const storedTheme = localStorage.getItem("theme");
 
-  if (storedTheme === "dark") {
-    doc.classList.add("dark")
-  }
-})
+	if (storedTheme === "dark") {
+		doc.classList.add("dark");
+	}
+});
 
 // theme toggle
 const themeToggle = document.getElementById("theme-toggle");
@@ -22,10 +22,31 @@ themeToggle.addEventListener("click", () => {
 });
 
 // hamburger toggle
-const nav = document.querySelector("#nav");
+const nav = document.querySelector("#nav ul");
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
 	nav.classList.toggle("show");
 	hamburger.classList.toggle("active");
+});
+
+document.querySelectorAll(".custom-select").forEach((select) => {
+	const selected = select.querySelector(".selected");
+	const options = select.querySelectorAll(".options li");
+
+	selected.addEventListener("click", () => {
+		select.classList.toggle("open");
+	});
+
+	options.forEach((option) => {
+		option.addEventListener("click", () => {
+			selected.textContent = option.textContent;
+			select.classList.remove("open");
+		});
+	});
+});
+
+document.getElementById("level").addEventListener("click", () => {
+	const selected = document.querySelector(".custom-select");
+	selected.classList.add("open");
 });
